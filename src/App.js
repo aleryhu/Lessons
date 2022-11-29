36fdb0853525afb49f1204b198fb97f4954e84d1
aleryhu <111310786+aleryhu@users.noreply.github.com>
2022-11-29 06:54:34 +0100

    Installed additional packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Final from "./tutorial/11-react-router";
+import Setup from "./tutorial/11-react-router/final";
 function App() {
   return (
     <div className="container">
-      <Final />
+      <Setup />
     </div>
   );
 }
